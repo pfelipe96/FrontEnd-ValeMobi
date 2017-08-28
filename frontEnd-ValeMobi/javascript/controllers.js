f6d85545851c4595c491f1bd9cfb7d865ef23560
Paulo Felipe <p.felipe@msn.com>
2017-08-28 19:11:10 -0300

    Atualizacao de MVC

--- frontEnd-ValeMobi/javascript/controllers.js
+++ frontEnd-ValeMobi/javascript/controllers.js
@@ -10,16 +10,37 @@ angular.module('starter.controllers', [])
     {cM : 'CD03', tM : 'Títulos Públicos', nM : 'Tesouro Direto', pM: 30.11},
   ];
 
+  $scope.tipoNegociacao =
+  [
+    {tN : 'Vender'},
+    {tN : 'Comprar'}
+  ];
+
   // Recuperar lista do DataBase
   $http.get('http://localhost:3000/recuperar-negociacoes').then(function(resposta){
-    $scope.obterLista = resposta.data;
+    var obterLista = resposta.data;
+    $scope.obterListaInvertido = obterLista.slice(0).reverse();
+    console.log($scope.obterListaInvertido);
   });
 
   // Enviar os dados para o DataBase
-  $scope.finalizar = function (dados) {
+  $scope.finalizar = function (dados, tipoN) {
+    var dataNow = new Date;
+    var dataNegociacao = String(dataNow.getUTCDate()+"/"+(dataNow.getMonth()+1) +"/"+ dataNow.getUTCFullYear()+" às "+dataNow.getHours()+":"+dataNow.getMinutes()+":"+dataNow.getSeconds());
+    dados.tN = tipoN;
+    dados.dN = dataNegociacao;
+
     $http.post('http://localhost:3000/negociacao', dados).then(function(resposta){
-      console.log("Funcao funcionando")
+        $route.reload('/');
+    }, function(resposta){
+
+      if(resposta.status == 403){
+        return $scope.erro = "Por favor, preencha todas as lacunas";
+      }
+
+      if(reposta.status == 503){
+        return $scope.erro = "Tente novamente mais tarde";
+      }
     });
-    $route.reload('/');
   };
 });
